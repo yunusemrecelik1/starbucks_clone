f98a3dd418031128d5e4a079782e44d75433e8f4
yunusemrecelik <yemrecelikofficial@gmail.com>
2022-01-10 23:02:00 +0300

    migrate null safety

--- lib/view/home/order/view/order_page_view.dart
+++ lib/view/home/order/view/order_page_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class OrderPage extends StatelessWidget {
-  const OrderPage({Key key}) : super(key: key);
+  const OrderPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/view/home/payment/payment_page.dart
+++ lib/view/home/payment/payment_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PaymentPage extends StatelessWidget {
-  const PaymentPage({Key key}) : super(key: key);
+  const PaymentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
